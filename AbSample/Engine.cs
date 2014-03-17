725790cf972686ef8ec86c57b362dda29ef03015
andreabalducci <mtb.snowboard@gmail.com>
2014-03-17 21:21:19 +0100

    initial commit

---
+++ AbSample/Engine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Core.Logging;
+
+namespace AbSample
+{
+    public class Engine
+    {
+        private ILogger _logger;
+
+        public ILogger Logger
+        {
+            get { return _logger ?? NullLogger.Instance; }
+            set { _logger = value; }
+        }
+
+        public void Start()
+        {
+            Logger.Debug("Started");
+        }
+
+        public void Stop()
+        {
+            Logger.Debug("Stopped");
+        }
+    }
+}
